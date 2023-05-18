636492cfa4a7475b4c70915400cc4b0168033366
WAS <jordanslost@gmail.com>
2023-05-18 10:22:41 -0700

    Add Max Size Support

--- ComfyImage.py
+++ ComfyImage.py
@@ -8,12 +8,22 @@ from io import BytesIO
 class ComfyLatentImage():
 
     @staticmethod
-    def saveComfyLatent(tensor, image, image_path):
+    def saveComfyLatent(tensor, image, image_path, mdim=1280):
         tensor_bytes = safetensors.torch.save(tensor)
         compressed_data = BytesIO()
         with zipfile.ZipFile(compressed_data, mode='w') as archive:
             archive.writestr("latent_tensor", tensor_bytes)
         image_with_metadata = image.copy()
+        width, height = image_with_metadata.size
+        if width > mdim or height > mdim:
+            aspect_ratio = width / height
+            if aspect_ratio > 1:
+                new_width = mdim
+                new_height = int(mdim / aspect_ratio)
+            else:
+                new_width = int(mdim * aspect_ratio)
+                new_height = mdim
+            image_with_metadata = image_with_metadata.resize((new_width, new_height), Image.ANTIALIAS)
         exif_data = piexif.load(image_with_metadata.info["exif"])
         exif_data["Exif"][piexif.ExifIFD.UserComment] = compressed_data.getvalue()
         exif_bytes = piexif.dump(exif_data)
