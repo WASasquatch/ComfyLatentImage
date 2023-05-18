6a2e81a6c2233dc32431346007c635b84c2f16b9
WAS <jordanslost@gmail.com>
2023-05-18 13:46:20 -0700

    Better PNG Optimization

--- ComfyImage.py
+++ ComfyImage.py
@@ -11,14 +11,14 @@ class ComfyLatentImageIO():
         self.mdim = mdim
         self.format = format
 
-    def save(self, tensor, image, image_path, mdim=1280, format='webp'):
+    def save(self, tensor, image, image_path, mdim=1280, format='webp', optimize_png=False):
         tensor = {"latent_tensor": tensor}
         tensor_bytes = safetensors.torch.save(tensor)
         compressed_data = BytesIO()
         with zipfile.ZipFile(compressed_data, mode='w') as archive:
             archive.writestr("latent_tensor", tensor_bytes)
-        image_with_metadata = image.copy()
-        width, height = image_with_metadata.size
+        exif_image = image.copy()
+        width, height = exif_image.size
         if width > mdim or height > mdim:
             aspect_ratio = width / height
             if aspect_ratio > 1:
@@ -27,18 +27,20 @@ class ComfyLatentImageIO():
             else:
                 new_width = int(mdim * aspect_ratio)
                 new_height = mdim
-            image_with_metadata = image_with_metadata.resize((new_width, new_height), Image.ANTIALIAS)
-        exif_data = piexif.load(image_with_metadata.info["exif"])
+            exif_image = exif_image.resize((new_width, new_height), Image.ANTIALIAS)
+        exif_data = piexif.load(exif_image.info["exif"])
         exif_data["Exif"][piexif.ExifIFD.UserComment] = compressed_data.getvalue()
         exif_bytes = piexif.dump(exif_data)
         
         file_ext = os.path.splitext(image_path)[1].lower()
         if file_ext == '.webp':
-            image_with_metadata.save(image_path, format='webp', exif=exif_bytes)
+            exif_image.save(image_path, format='webp', exif=exif_bytes)
         elif file_ext == '.png':
             png_info = PngImagePlugin.PngInfo()
             png_info.add_text("UserComment", compressed_data.getvalue())
-            image_with_metadata.save(image_path, format='png', exif=exif_bytes, pnginfo=png_info, optimized=True)
+            if optimize_png:
+                exif_image = exif_image.convert("P", palette=Image.ADAPTIVE, colors=256)
+            exif_image.save(image_path, format='png', exif=exif_bytes, pnginfo=png_info, optimize=(True if optimize_png else False))
         else:
             raise ValueError("Invalid file extension. Only '.webp' and '.png' extensions are supported.")
 
@@ -50,5 +52,5 @@ class ComfyLatentImageIO():
             with zipfile.ZipFile(compressed_data_io, mode='r') as archive:
                 tensor_bytes = archive.read("latent_tensor")
             tensor = safetensors.torch.load(tensor_bytes)
-            return tensor
+            return tensor['latent_tensor']
         return None
